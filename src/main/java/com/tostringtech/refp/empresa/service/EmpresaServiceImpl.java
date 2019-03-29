0d9c6da112a7c13a1120b93087d64c2c7bcae35c
John Paiva <john.icm09@gmail.com>
2019-03-29 00:35:48 -0300

    feature 17 e 23 - Criação dos métodos de cadastro e listagem do endpoint empresa

--- src/main/java/com/tostringtech/refp/empresa/service/EmpresaServiceImpl.java
+++ src/main/java/com/tostringtech/refp/empresa/service/EmpresaServiceImpl.java
@@ -1,9 +1,50 @@
 package com.tostringtech.refp.empresa.service;
 
+import com.tostringtech.refp.application.exceptions.ObjectNotFoundException;
+import com.tostringtech.refp.application.model.Empresa;
+import com.tostringtech.refp.empresa.repository.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmpresaServiceImpl implements EmpresaService {
 
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
+
+    @Override
+	public List<Empresa> findAll() {
+		return (List<Empresa>) empresaRepository.findAll();
+	}
+    @Override
+    public Optional<Empresa> findById(Long codigo) {
+        //Empresa empresa = empresaRepository.findById(codigo);
+        return empresaRepository.findById(codigo);
+    }
+
+    @Override
+    public Page<Empresa> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        return null;
+    }
+
+    @Override
+    public Empresa create (Empresa empresa){
+        return empresaRepository.save(empresa);
+    }
+
+    public Empresa update(Empresa empresa) {
+        findById(empresa.getCodigo());
+        return empresaRepository.save(empresa);
+    }
+    @Override
+    public void delete(Long codigo)  {
+        findById(codigo);
+        empresaRepository.deleteById(codigo);
+    }
 
 }
