7ed7fb03bbf5fe78e7cacecad2c6f38165bef514
Alberes Junior <alberesjr22@gmail.com>
2020-04-23 21:37:28 -0300

    Correcoes e insersoes de despesas

--- src/main/java/com/tostringtech/refp/expense/rest/controller/ExpenseRestController.java
+++ src/main/java/com/tostringtech/refp/expense/rest/controller/ExpenseRestController.java
@@ -1,13 +1,16 @@
 package com.tostringtech.refp.expense.rest.controller;
 
+import com.tostringtech.refp.application.exceptions.ObjectNotFoundException;
 import com.tostringtech.refp.application.models.Despesa;
-import com.tostringtech.refp.application.models.Projeto;
-import com.tostringtech.refp.enterprise.api.resources.EnterpriseResource;
+import com.tostringtech.refp.expense.api.repository.ExpenseRepository;
 import com.tostringtech.refp.expense.api.resources.ExpenseResource;
 import com.tostringtech.refp.expense.api.service.ExpenseService;
-import com.tostringtech.refp.project.api.resources.ProjectResource;
+//import com.tostringtech.refp.reports.ReportService;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -21,12 +24,20 @@ import java.util.stream.Collectors;
 @Api(tags = "Expense")
 public class ExpenseRestController {
 
+    private static final Logger logger = Logger.getLogger(ExpenseRestController.class);
+
     @Autowired
     private ExpenseService expenseService;
+    
+    @Autowired
+    private ExpenseRepository repo;
+    
+//    @Autowired
+//    private ReportService reportService;
 
     @PostMapping("/expenses")
     @ApiOperation(tags = {"Expense"}, value = "Cadastrar uma nova Despesa ")
-    public ResponseEntity<ExpenseResource> createProject(@RequestBody ExpenseResource resource) {
+    public ResponseEntity<ExpenseResource> createExpense(@RequestBody ExpenseResource resource) {
         Despesa despesa = expenseService.create(new Despesa(resource));
         resource = new ExpenseResource(despesa);
         return new ResponseEntity<>(resource, HttpStatus.CREATED);
@@ -34,43 +45,60 @@ public class ExpenseRestController {
 
     @GetMapping("/expenses")
     @ApiOperation(tags = {"Expense"}, value = "Listar despesas cadastradas")
-    public ResponseEntity<List<ExpenseResource>> listProjects(Pageable pageable) {
+    public ResponseEntity<List<ExpenseResource>> listExpenses(Pageable pageable) {
 
         List<ExpenseResource> resources = expenseService
-                .findAll()
+                .findAll(pageable)
                 .stream()
                 .map(ExpenseResource::new)
                 .collect(Collectors.toList());
 
         if (!resources.isEmpty()) {
-            return new ResponseEntity<>(resources, HttpStatus.OK);
+        	return ResponseEntity.ok(resources);
         }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return (ResponseEntity<List<ExpenseResource>>) ResponseEntity.noContent();
     }
 
     @GetMapping("/expenses/{id}")
     @ApiOperation(tags = {"Expense"}, value = "Encontrar uma Despesas")
-    public ResponseEntity<ExpenseResource> findProjectById(@PathVariable Long id) {
+    public ResponseEntity<ExpenseResource> findExpenseById(@PathVariable Long id) {
         Despesa despesa = expenseService.findById(id).orElse(null);
-        ExpenseResource resource = new ExpenseResource(despesa);
-
-        if (resource != null) {
-            return new ResponseEntity<>(resource, HttpStatus.OK);
+        if (despesa != null) {
+        	ExpenseResource resource = new ExpenseResource(despesa);
+        	return ResponseEntity.ok(resource);
         }
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
+    
     @PutMapping(value = "/expenses/{id}")
     @ApiOperation(tags = {"Expense"}, value = "Atualizar uma Despesa")
-    public ResponseEntity<Void> updateProject(@RequestBody ProjectResource resoruce, @PathVariable Long id) {
-        //TODO
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Despesa> updateExpense(@RequestBody ExpenseResource resource, @PathVariable Long id){
+    	Despesa despesa = expenseService.findById(id).orElseThrow(() -> {
+            return new ObjectNotFoundException("Despesa n�o encontrada");
+        });
+    	if(resource.getData() != null) {
+    		despesa.setData(resource.getData());
+    	}
+    	if (resource.getValue() != 0D) {
+    		despesa.setValor(resource.getValue());;
+    	}
+    	if(resource.getDocumentNumber() != null) {
+    		despesa.setNumeroDocumento(resource.getDocumentNumber());    		
+    	}
+    	despesa = expenseService.update(despesa);
+    	return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(value = "/expenses/{id}")
     @ApiOperation(tags = {"Expense"}, value = "Deletar uma Despesa")
     public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
-        //TODO
+    	expenseService.delete(id);
         return ResponseEntity.noContent().build();
     }
+    
+//    @GetMapping(value = "/expenses/report/")
+//    public String generateExpenseReport(Pageable pageable) throws FileNotFoundException, JRException{
+//    	return reportService.exportReport(pageable);
+//    }
 }
